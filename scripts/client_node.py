90b6eddbb95ccfdf4b82804e37699a192ad86182
Nic McCullough <mccullnj@dukes.jmu.edu>
2016-03-15 15:01:32 -0400

    trying this again.. added pickling

--- scripts/client_node.py
+++ scripts/client_node.py
@@ -3,23 +3,40 @@ import rospy
 import os
 import pickle
 from socket import *
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import PoseWithCovariance
 
+        # in client_node
+            # subscribe to odom topic
+            # in subscribe callback:
+                # pull out just the info we need (pose probably)
+                # pickle it using pickle.dumps(obj_to_pickle)
+                # store pickle in global variable
+            # in main loop:
+                # send pickle string through the UDP socket to the server at some interval
 class ClientNode():
     def __init__(self):
-        host = "134.126.125.125" # ip of server
+        rospy.init_node('client_node')
+        host = "127.0.0.1" # ip of server
         port = 13000
         self.addr = (host, port)
         self.UDPSock = socket(AF_INET, SOCK_DGRAM)
-        
-    def run(self):
-        while True:
-            data = raw_input("Enter message to send or type 'exit': ")
+        self.pickled_str = ""
+        rospy.Subscriber('/odom', Odometry, self.odom_callback)
+
+        rate = rospy.Rate(1)
+        while not rospy.is_shutdown():
+            data = self.pickled_str
             self.UDPSock.sendto(data, self.addr)
             if data == "exit":
                 break
+            rate.sleep()
+
         self.UDPSock.close()
         os._exit(0)
+
+    def odom_callback(self, odom_msg):
+        self.pickled_str = pickle.dumps(odom_msg.pose)
         
 if __name__ == "__main__":
-    node = ClientNode()
-    node.run()
+    ClientNode()
--- scripts/server_node.py
+++ scripts/server_node.py
@@ -6,36 +6,27 @@ from socket import *
 
 class ServerNode():
     def __init__(self):
+        rospy.init_node('server_node')
         host = ""
         port = 13000
         self.buf = 1024
         self.addr = (host, port)
         self.UDPSock = socket(AF_INET, SOCK_DGRAM)
         self.UDPSock.bind(self.addr)
-        # in client_node
-            # subscribe to odom topic
-            # in subscribe callback:
-                # pull out just the info we need (pose probably)
-                # pickle it using pickle.dumps(obj_to_pickle)
-                # store pickle in global variable
-            # in main loop:
-                # send pickle string through the UDP socket to the server at some interval
         # in server_node
             # have loop that listens to client_node
                 # when message received, de-pickle using pickle.loads(string_to_depickle)
                 # store in global variable
             # probably create rviz marker from pose of other robot and publish to topic 
         
-    def run(self):
-        print "Waiting to receive messages..."
         while True:
             (data, self.addr) = self.UDPSock.recvfrom(self.buf)
-            print "Received message: " + data
+            depickled_str = pickle.loads(data)
+            rospy.loginfo(depickled_str)
             if data == "exit":
                 break
         self.UDPSock.close()
         os._exit(0)
                 
 if __name__ == "__main__":
-    node = ServerNode()
-    node.run()
+    ServerNode()
