fd038d1a936b61572c05583f07918ddd9cc77206
Joseph Neidigh <neidigjs@dukes.jmu.edu>
2016-04-13 22:41:38 -0400

    ddddd

--- scripts/location_client.py
+++ scripts/location_client.py
@@ -6,52 +6,49 @@ import socket
 import numpy as np
 from visualization_msgs.msg import Marker
 from geometry_msgs.msg import PoseWithCovarianceStamped, Point, PointStamped
+import tf
+
 
 # This node will periodically send its position to the CC.
 # It will also send a list of the messes that it has discovered to the CC.
 class ClientNode():
     def __init__(self):
-        rospy.init_node('client_node')
+        rospy.init_node('location_node')
         rospy.Subscriber('/visualization_marker', Marker, self.marker_callback)
+        rospy.Subscriber('/amcl_pose', PoseWithCovarianceStamped,
+                            self.position_callback)
 
         host = "134.126.125.125" # ip of server
         port = 13000
-        addr = (host, port)
+        self.addr = (host, port)
         self.messes = []
         self.tf_listener = tf.TransformListener()
-
+        self.position = None
+        self.count = 0
         try:
-            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         except:
             rospy.loginfo("FAILED TO CREATE SOCKET")
             sys.exit()
 
         rospy.Subscriber('/amcl_pose', PoseWithCovarianceStamped, self.amcl_callback)
-        ROBOT_ID = 1
+        self.ROBOT_ID = 0
         self.pos = [0,0]
         self.mess = [None,None]
         rate = rospy.Rate(5)
 
         rospy.loginfo("LOCATION_CLIENT SETUP COMPLETE")
         self.old_marker = Point()
-        self.old_marker.x = 0
-        self.old_marker.y = 0
+        self.old_marker.x = 100000
+        self.old_marker.y = 100000
         while not rospy.is_shutdown():
             mess_x = self.mess[0]
             mess_y = self.mess[1]
 
             try:
-                s.sendto(pickle.dumps([0, ROBOT_ID, self.pos[0], self.pos[1]]), addr)
-##############################################################################################
-#                      NEED TO SEND THEM ONLY IF THEY ARE DIFFERENT MESSES BECAUSE THIS CLIENT
-#                      SENDS MESS IN ROBOT FRAME AND THE MESS_X AND MESS_Y WILL CHANGE WHEN
-#                      THE ROBOT MOVES 
-##############################################################################################
-                if mess_x != None and mess_y != None:
-                    rospy.loginfo("HERE!!!!!!")
-                    s.sendto(pickle.dumps([1, ROBOT_ID, mess_x, mess_y]), addr)
-                    old_mess_x = mess_x
-                    old_mess_y = mess_y
+                self.s.sendto(pickle.dumps([0,self.ROBOT_ID, self.pos[0], self.pos[1]]), self.addr)
+
+ 
             except Exception as e:
                 s.close()
                 rospy.loginfo("LOCATION_CLIENT ERROR. CLOSING SOCKET")
@@ -70,7 +67,7 @@ class ClientNode():
 
     def marker_callback(self, marker):
         """ The marker callback to see the ar_markers"""
-        if not self.close_enough(self.old_marker.x, self.old_marker.y, marker.pose.position.x, marker.pose.position.y, .15):
+        if self.count == 100:
             marker_point = PointStamped()
             marker_point.header.frame_id = 'camera_rgb_optical_frame'
             marker_point.header.stamp = rospy.get_rostime()
@@ -90,16 +87,33 @@ class ClientNode():
                 print(e)
                 print("ERROR in marker_callback")
 
-            if self.close_enough(self.position.position.x, self.position.position.y, marker_point.point.x, marker_point.point.y, 1):
-                self.mess[0] = marker_point.point.x
-                self.mess[1] = marker_point.point.y
-                self.old_marker.x = marker_point.point.x
-                self.old_marker.y = marker_point.point.y
+            new = True
+            if len(self.messes) is not 0:
+                for mess in self.messes:
+                    if (self.close_enough(mess.x, mess.y, marker_point.point.x, marker_point.point.y, .15)):
+                        new = False
+            else:
+                if self.close_enough(self.position.position.x, self.position.position.y, marker_point.point.x, marker_point.point.y, 1):
+                    self.messes.append(marker_point.point)
+                    self.s.sendto(pickle.dumps([1, self.ROBOT_ID, marker_point.point.x, marker_point.point.y]), self.addr)
+
+            if self.close_enough(self.position.position.x, self.position.position.y, marker_point.point.x, marker_point.point.y, 1) and new:
+                self.messes.append(marker_point.point)
+                self.s.sendto(pickle.dumps([1, self.ROBOT_ID, marker_point.point.x, marker_point.point.y]), self.addr)
+
+            self.count == 0
+        else:
+            self.count += 1
+            
+                
 
     def close_enough(self, x_one, y_one, x_two, y_two, threshold):
         """ Checks to see if its close enough to a goal"""
         return (np.sqrt((x_one - x_two)**2 + (y_one - y_two)**2) < threshold)
-            
+           
+    def position_callback(self, pos):
+        """ Saves the current position of the robot"""
+        self.position = pos.pose.pose 
         
 if __name__ == "__main__":
     ClientNode()
